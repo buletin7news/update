441cb520dc78e5ff35a277cb5ad3dc9639fc08b3
buletin7news <jagustian018@gmail.com>
2024-11-23 19:32:21 +0700

    Update haha.php

--- haha.php
+++ haha.php
@@ -1,79 +1,66 @@
-<!DOCTYPE html>
+<?php
 
-<div style="display:none">
-<a href="https://onenews.sch.id/sejarah-dan-keunikan-candi-gatotkaca-dieng/" style="display:none">Gatotkaca</a><br>
-<a href="https://narsis.id/ada-juga-jabat-tangan-di-konser-jkt48-tahun-2013/" style="display:none">JKT48</a><br>
-<a href="https://pansos.id/tempat-wisata-indonesia-di-takengon-aceh-tengah-dengan-ciri-khas-kopi-gayo/" style="display:none">Kopi Gayo</a><br>
-<a href="https://forensik.id/satria-muda-britama-jakarta-raih-gelar-juara-liga-bola-basket-indonesia-ibl-2004/" style="display:none">Juara Liga Bola Basket</a><br>
-<a href="https://mainstream.id/game-hp-yang-cocok-untuk-anak-anak/" style="display:none">Game HP yang Cocok untuk Anak-Anak</a><br>
-<a href="https://balqisnews.sch.id/10-game-ramah-anak-tahun-2024/" style="display:none">10 Game Ramah Anak Tahun 2024</a><br>
-<a href="https://harpitnas.com/harpitnas-hari-kejepit-nasional/" style="display:none">Harpitnas</a><br>
-<a href="https://muntaber.com/muntaber-muntah-dan-berak/" style="display:none">Muntaber</a><br>
-<a href="https://hardiknas.com/peringatan-hardiknas-hari-pendidikan-nasional-untuk-kemajuan-dan-kesejahteraan-bangsa/" style="display:none">Hardiknas</a><br>
-<a href="https://paspampres.com/paspampres-penjaga-kehormatan-keamanan-presiden/" style="display:none">Paspampres</a><br>
-<a href="https://pakcoy.com/sayur-pakcoy-manfaat-cara-menanam-dan-mengolah-resep-lezat/" style="display:none">Pakcoy</a><br>
-<a href="https://harkitnas.com" style="display:none">Harkitnas</a><br>
-<a href="https://tangkubanperahu.com" style="display:none">Tangkuban Perahu</a><br>
-<a href="https://sibolangit.com" style="display:none">Sibolangit</a><br>
-<a href="https://simarjarunjung.com" style="display:none">Simarjarunjung</a><br>
-<a href="https://siguragura.com" style="display:none">Sigura-gura</a><br>
-<a href="https://simanindo.com" style="display:none">Simanindo</a><br>
-<a href="https://padarincang.com" style="display:none">Padarincang</a><br>
-<a href="https://cilawu.com" style="display:none">Cilawu</a><br>
-<a href="https://cilengkrang.com" style="display:none">Cilengkrang</a><br>
-<a href="https://kolektor.id" style="display:none">Kolektor</a><br>
-<a href="https://pelukis.id" style="display:none">Pelukis</a><br>
-<a href="https://pancoran.id" style="display:none">Pancoran</a><br>
-<a href="https://kualitas.id" style="display:none">Kualitas</a><br>
-<a href="https://jasmani.id" style="display:none">Jasmani</a><br>
-<a href="https://cipanas.id" style="display:none">Cipanas</a><br>
-<a href="https://eksklusif.id" style="display:none">Eksklusif</a>
-<a href="https://inovatif.id" style="display:none">Inovatif</a><br>
-<a href="https://xenia.id" style="display:none">Xenia</a><br>
-<a href="https://wamena.id" style="display:none">Wamena</a><br>
-<a href="https://parapat.id" style="display:none">Parapat</a><br>
-<a href="https://penatapan.id" style="display:none">Penatapan</a><br>
-<a href="https://balige.id" style="display:none">Balige</a><br>
-<a href="https://ciomas.id" style="display:none">Ciomas</a><br>
-<a href="https://aaatrucksandautowreckings.com" style="display:none;">https://aaatrucksandautowreckings.com</a><br>
-<a href="https://narentechnologies.com" style="display:none;">https://narentechnologies.com</a><br>
-<a href="https://youthlinkjamica.com" style="display:none;">https://youthlinkjamica.com</a><br>
-<a href="https://arbirate.com" style="display:none;">https://arbirate.com</a><br>
-<a href="https://playoutworlder.com" style="display:none;">https://playoutworlder.com</a><br>
-<a href="https://temeculabluegrass.com" style="display:none;">https://temeculabluegrass.com</a><br>
-<a href="https://eldesigners.com" style="display:none;">https://eldesigners.com</a><br>
-<a href="https://cheklani.com" style="display:none;">https://cheklani.com</a><br>
-<a href="https://totodal.com" style="display:none;">https://totodal.com</a><br>
-<a href="https://kmspicodownloads.com" style="display:none;">https://kmspicodownloads.com</a><br>
-<a href="https://apkcrave.com" style="display:none;">https://apkcrave.com</a><br>
-<a href="https://bestcarinsurancewsa.com" style="display:none;">https://bestcarinsurancewsa.com</a><br>
-<a href="https://complidia.com" style="display:none;">https://complidia.com</a><br>
-<a href="https://eveningupdates.com" style="display:none;">https://eveningupdates.com</a><br>
-<a href="https://livingauberean.com" style="display:none;">https://livingauberean.com</a><br>
-<a href="https://mcochacks.com" style="display:none;">https://mcochacks.com</a><br>
-<a href="https://mostcreativeresumes.com" style="display:none;">https://mostcreativeresumes.com</a><br>
-<a href="https://oxcarttavern.com" style="display:none;">https://oxcarttavern.com</a><br>
-<a href="https://riceandshinebrunch.com" style="display:none;">https://riceandshinebrunch.com</a><br>
-<a href="https://shoesknowledge.com" style="display:none;">https://shoesknowledge.com</a><br>
-<a href="https://topthreenews.com" style="display:none;">https://topthreenews.com</a><br>
+/**
+ * @mainpage OJS API Reference
+ *
+ * Welcome to the OJS API Reference. This resource contains documentation
+ * generated automatically from the OJS source code.
+ *
+ * The design of Open %Journal Systems 2.x is heavily structured for
+ * maintainability, flexibility and robustness. For this reason it may seem
+ * complex when first approached. Those familiar with Sun's Enterprise Java
+ * Beans technology or the Model-View-Controller (MVC) pattern will note many
+ * similarities.
+ *
+ * As in a MVC structure, data storage and representation, user interface
+ * presentation, and control are separated into different layers. The major
+ * categories, roughly ordered from "front-end" to "back-end," follow:
+ * - Smarty templates, which are responsible for assembling HTML pages to
+ *   display to users;
+ * - Page classes, which receive requests from users' web browsers, delegate
+ *   any required processing to various other classes, and call up the
+ *   appropriate Smarty template to generate a response;
+ * - Action classes, which are used by the Page classes to perform non-trivial
+ *   processing of user requests;
+ * - Model classes, which implement PHP objects representing the system's
+ *   various entities, such as Users, Articles, and Journals;
+ * - Data Access Objects (DAOs), which generally provide (amongst others)
+ *   update, create, and delete functions for their associated Model classes,
+ *   are responsible for all database interaction;
+ * - Support classes, which provide core functionalities, miscellaneous common
+ *
+ * As the system makes use of inheritance and has consistent class naming
+ * conventions, it is generally easy to tell what category a particular class
+ * falls into.
+ * For example, a Data Access Object class always inherits from the DAO class,
+ * has a Class name of the form [Something]%DAO, and has a filename of the form
+ * [Something]%DAO.inc.php.
+ *
+ * To learn more about developing OJS, there are several additional resources
+ * that may be useful:
+ * - The docs/README document
+ * - The PKP support forum at http://pkp.sfu.ca/support/forum
+ * - The technical reference (and other documents), available at
+ *   http://pkp.sfu.ca/ojs_documentation
+ *
+ * @file index.php
+ *
+ * Copyright (c) 2013-2014 Simon Fraser University Library
+ * Copyright (c) 2003-2014 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup index
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
+ */
 
-<a href="https://pemilu.posokab.go.id" style="display:none">Mahjong Ways</a><br>
-</div>
+// Initialize global environment
+define('INDEX_FILE_LOCATION', __FILE__);
+require('./lib/pkp/includes/bootstrap.inc.php');
 
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Deskripsi singkat halaman Anda">
-    <meta name="keywords" content="kata kunci 1, kata kunci 2, kata kunci 3">
-    <meta name="author" content="Nama Anda atau Brand Anda">
-    <title></title>
-    <!-- Link CSS eksternal -->
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    
-   
-</body>
-</html>
+// Serve the request
+$application =& PKPApplication::getApplication();
+$application->execute();
+
+?>
